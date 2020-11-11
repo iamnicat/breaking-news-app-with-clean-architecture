f1701963a4df17eb5caf2032af3b99500cd01fc0
Nicat <iamnicathaciyev13@gmail.com>
2020-11-11 13:52:34 +0400

    Article data class is created

---
+++ app/src/main/java/com/nicathaciyev/newsappwithmvvm/ui/Article.kt
@@ -0,0 +1,20 @@
+package com.nicathaciyev.newsappwithmvvm.ui
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "articles")
+data class Article(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
+    val author: String,
+    val content: String,
+    val description: String,
+    val publishedAt: String,
+    val source: Source,
+    val title: String,
+    val url: String,
+    val urlToImage: String
+)
