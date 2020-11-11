84a83e01fc3b576facfea8ca32046c9a0f76df7a
Nicat <iamnicathaciyev13@gmail.com>
2020-11-11 12:51:18 +0400

    NewsAPI interface is created

---
+++ app/src/main/java/com/nicathaciyev/newsappwithmvvm/api/NewsAPI.kt
@@ -0,0 +1,31 @@
+package com.nicathaciyev.newsappwithmvvm.api
+
+import com.nicathaciyev.newsappwithmvvm.ui.NewsResponse
+import com.nicathaciyev.newsappwithmvvm.utils.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+}
